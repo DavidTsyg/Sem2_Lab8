a79c88d303a92a86941202afd7619504ebdd6adb
DavidTsyg <davidtsyg@gmail.com>
2017-05-19 02:54:01 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "Sem2_Lab8.hpp"
+using namespace std;
+
+int main()
+{
+	Complex<double> num1(1, 5)
+  Complex<double> num2(3, 4)
+	cout << num1 * num2;
+	return 0;
+}
